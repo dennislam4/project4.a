48aefecb0a00de1a9c387ef003b9d1563e5a20ac
dennislam4 <107871794+dennislam4@users.noreply.github.com>
2022-07-05 11:28:48 -0600

    Initial commit

---
+++ fall_distance.py
@@ -0,0 +1,14 @@
+# Author: Dennis Lam
+# GitHub username: dennislam4
+# Date: 7/05/2022
+# Description: A function that determines the distance an object falls due to gravity in a specific time period.
+
+gravity = 9.8
+
+
+def fall_distance(time):
+    dist = ((1 / 2) * gravity * time ** 2)
+    return dist
+
+
+
